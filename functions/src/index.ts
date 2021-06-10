4f4c9a6889c8c570551f1965d53982b0aefee8ab
obrown <odain.brown@alvys.com>
2021-06-10 12:11:08 -0500

    gas station routes added

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,7 @@ import * as functions from "firebase-functions";
 import * as express from 'express';
 import  {addUser, deleteUser, getAllUsers, updateUser } from './Controllers/userController';
 import {validateFirebaseIdToken} from './Authentication/authMiddleware';
+import { addGasStation, getAllGasStations } from "./Controllers/gasStationController";
 
 const app = express();
 //This will apply the middleware to all request
@@ -9,7 +10,7 @@ const app = express();
 app.use(validateFirebaseIdToken);
 
 
-//Routes
+//User Routes Routes
 app.post('/addUser',addUser);
 
 app.get('/allUsers',getAllUsers);
@@ -17,5 +18,11 @@ app.get('/allUsers',getAllUsers);
 app.patch('/updateUser/:userId',updateUser);
 
 app.delete('/deleteUser/:userId',deleteUser);
+
+
+//GAs Stations routes
+app.post('/addGasStation',addGasStation);
+
+app.get('/allGasStations',getAllGasStations);
 
 exports.app = functions.https.onRequest(app);
